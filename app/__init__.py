5d2998ad01957400af1bd6519594258c3aa8cdbb
Djain-13 <jain.daksh138@gmail.com>
2025-09-17 10:50:48 +0530

    Login

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
+# IMPORTANT: Generate your own unique secret key
+app.config['SECRET_KEY'] = '185b9c36ee1fdec423afe07eb20873a0' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' # Redirect non-logged-in users to the login page
 
 from app import routes
